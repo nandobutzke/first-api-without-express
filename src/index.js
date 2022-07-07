3b0da947ee0635947652b2875bc1bafb668ef2c6
Fernando Butzke <nandoproject209@gmail.com>
2022-07-07 12:56:03 -0300

    fisrt commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+
+const routes = require('./routes');
+
+const { URL } = require('url');
+
+const bodyParser = require('./helpers/bodyParser');
+
+const server = http.createServer((request, response) => {
+    const parsedUrl = new URL(`http://localhost:3333${request.url}`)
+
+    let { pathname } = parsedUrl;
+    let id = null;
+
+    const splitEndpoint = pathname.split('/').filter(Boolean);
+
+    if (splitEndpoint.length > 1) {
+        pathname = `/${splitEndpoint[0]}/:id`;
+        id = splitEndpoint[1];
+    }
+
+    const route = routes.find((routeObj) => (
+        routeObj.endpoint === pathname && routeObj.method === request.method
+    ))
+
+    if (route) {
+        request.query = Object.fromEntries(parsedUrl.searchParams);
+        request.params = { id };
+
+        response.send = (statusCode, body) => {
+            response.writeHead(statusCode, { 'Content-type': 'application/json' });
+            response.end(JSON.stringify(body));
+        }
+
+        if (['POST', 'PUT', 'PATCH'].includes(request.method)) {
+            bodyParser(request, () => route.handler(request, response));
+        } else {
+            route.handler(request, response);
+        }
+    } else {
+        response.writeHead(404, { 'Content-type': 'text/html' });
+        response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+    }
+});
+
+server.listen(3333, console.log("✔ Server started at http://localhost:3333"));
---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const UserController = require('./controllers/UserController');
+
+module.exports = [
+    {
+        method: 'GET',
+        endpoint: '/users',
+        handler: UserController.listUsers
+    },
+    {
+        method: 'GET',
+        endpoint: '/users/:id',
+        handler: UserController.getUserById
+    },
+    {
+        method: 'POST',
+        endpoint: '/users',
+        handler: UserController.createUser
+    },
+    {
+        method: 'PUT',
+        endpoint: '/users/:id',
+        handler: UserController.updateUser
+    },
+    {
+        method: 'DELETE',
+        endpoint: '/users/:id',
+        handler: UserController.deleteUser
+    },
+];
